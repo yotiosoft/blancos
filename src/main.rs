5132d19ec201fb64540b078259d9ee0fa4635353
yotiosoft <ytani0323@gmail.com>
2024-10-02 16:57:10 +0900

    impl print! & println! macros

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,7 @@ static HELLO: &[u8] = b"Hello World!";
 /// no_mangle: 関数名を変更しない. エントリポイントをリンカに伝えるために必須
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
     
     loop{}
 }
