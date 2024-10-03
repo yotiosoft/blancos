1208f530d2e3a434a152a9286bfa3363ca03c4e1
yotiosoft <ytani0323@gmail.com>
2024-10-03 13:46:26 +0900

    impl unit test & test library

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,23 @@
 #![no_main]     // main 関数を使わない
 
 #![feature(custom_test_frameworks)] 
-#![test_runner(crate::test_runner)]
+#![test_runner(blancos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use blancos::println;
 
-mod vga_buffer;
-mod serial;
+/// エントリポイント
+/// no_mangle: 関数名を変更しない. エントリポイントをリンカに伝えるために必須
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+    
+    loop {}
+}
 
 /// パニックハンドラ
 /// パニック時に呼ばれる
@@ -23,62 +33,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-/// エントリポイント
-/// no_mangle: 関数名を変更しない. エントリポイントをリンカに伝えるために必須
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
-    test_main();
-    
-    loop {}
-}
-
-// QEMU Exit Code
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-/// test_runner
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    
-    exit_qemu(QemuExitCode::Success);
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
+    blancos::test_panic_handler(info)
 }
