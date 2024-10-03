ff579d24b2cfa1cecd03575ff2e8414843a684ee
yotiosoft <ytani0323@gmail.com>
2024-10-04 04:36:41 +0900

    none harness test

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,8 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(blancos::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use blancos::{QemuExitCode, exit_qemu, serial_println, serial_print};
@@ -16,24 +13,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
 
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn should_panic() {
+fn should_fail() {
     serial_print!("should_panic::should_fail...\t");
     assert_eq!(0, 1);
 }
