a35a6cff77d07ad5489a9f30a8e844089b9b025e
yotiosoft <ytani0323@gmail.com>
2025-05-28 07:36:59 +0900

    dynamic-memory

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-#![feature(abi_x86_interrupt)]
+#![feature(abi_x86_interrupt)
+
+extern crate alloc;
 
 use core::panic::PanicInfo;
 
