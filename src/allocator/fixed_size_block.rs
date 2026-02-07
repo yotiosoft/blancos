28a0f3401e0862f847c9ff34934b01bb18d2fe0d
yotiosoft <ytani0323@gmail.com>
2026-02-08 00:33:04 +0900

    impl FixedSizeBlockAllocator

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -1,3 +1,6 @@
+use core::{ mem, ptr, ptr::NonNull };
+use super::Locked;
+use alloc::alloc::{ Layout, GlobalAlloc };
 
 struct ListNode {
     next: Option<&'static mut ListNode>,
@@ -28,6 +31,74 @@ impl FixedSizeBlockAllocator {
     pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
         unsafe {
             self.fallback_allocator.init(heap_start, heap_size);
+        }
+    }
+
+    // 代替アロケータを使って割り当て
+    fn fallback_alloc(&mut self, layout: Layout) -> *mut u8 {
+        match self.fallback_allocator.allocate_first_fit(layout) {
+            Ok(ptr) => ptr.as_ptr(),
+            Err(_) => ptr::null_mut(),
+        }
+    }
+}
+
+/// 与えられたレイアウトに対して適切なブロックサイズを選ぶ
+/// 
+/// `BLOCK_SIZES` 配列のインデックスを返す
+fn list_index(layout: &Layout) -> Option<usize> {
+    let required_block_size = layout.size().max(layout.align());
+    BLOCK_SIZES.iter().position(|&s| s >= required_block_size)
+}
+
+unsafe impl GlobalAlloc for Locked<FixedSizeBlockAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut allocator = self.lock();
+
+        match list_index(&layout) {
+            Some(index) => {
+                match allocator.list_heads[index].take() {
+                    Some(node) => {
+                        allocator.list_heads[index] = node.next.take();
+                        node as *mut ListNode as *mut u8
+                    }
+                    None => {
+                        // リストにブロックがない場合、新しいブロックを割り当てる
+                        let block_size = BLOCK_SIZES[index];
+                        // すべてのブロックサイズが2の塁上であるときのみ正しく動く
+                        let block_align = block_size;
+                        let layout = Layout::from_size_align(block_size, block_align).unwrap();
+                        allocator.fallback_alloc(layout)
+                    }
+                }
+            }
+            None => allocator.fallback_alloc(layout),
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        let mut allocator = self.lock();
+        
+        match list_index(&layout) {
+            Some(index) => {
+                let new_node = ListNode {
+                    next: allocator.list_heads[index].take(),
+                };
+                // ブロックがノードを格納できるサイズとアライメントを持っているか確認
+                assert!(mem::size_of::<ListNode>() <= BLOCK_SIZES[index]);
+                assert!(mem::align_of::<ListNode>() <= BLOCK_SIZES[index]);
+                let new_node_ptr = ptr as *mut ListNode;
+                unsafe {
+                    new_node_ptr.write(new_node);
+                    allocator.list_heads[index] = Some(&mut *new_node_ptr);
+                }
+            }
+            None => {
+                let ptr = NonNull::new(ptr).unwrap();
+                unsafe {
+                    allocator.fallback_allocator.deallocate(ptr, layout);
+                }
+            }
         }
     }
 }
