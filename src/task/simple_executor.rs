0a35c8d4b75110223f1ae6f8e6d07b8c99245d33
yotiosoft <ytani0323@gmail.com>
2026-02-12 01:06:17 +0900

    done impl simple_executor

--- src/task/simple_executor.rs
+++ src/task/simple_executor.rs
@@ -1,6 +1,6 @@
 use super::Task;
 use alloc::{ collections::VecDeque, vec::Vec };
-use core::task::{ Waker, RawWaker };
+use core::task::{ Context, RawWaker, RawWakerVTable, Waker, Poll };
 
 pub struct SimpleExecutor {
     task_queue: VecDeque<Task>,
@@ -16,10 +16,27 @@ impl SimpleExecutor {
     pub fn spawn(&mut self, task: Task) {
         self.task_queue.push_back(task)
     }
+
+    pub fn run(&mut self) {
+        while let Some(mut task) = self.task_queue.pop_front() {
+            let waker = dummy_waker();
+            let mut context = Context::from_waker(&waker);
+            match task.poll(&mut context) {
+                Poll::Ready(()) => {}       // task 完了
+                Poll::Pending => self.task_queue.push_back(task),
+            }
+        }
+    }
 }
 
 fn dummy_raw_waker() -> RawWaker {
-    todo!();
+    fn no_op(_: *const ()) {}
+    fn clone(_: *const ()) -> RawWaker {
+        dummy_raw_waker()
+    }
+
+    let vtable = &RawWakerVTable::new(clone, no_op, no_op, no_op);
+    RawWaker::new(0 as *const (), vtable)
 }
 
 fn dummy_waker() -> Waker {
