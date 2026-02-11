5d538d0fafecbac0b14cc59aa2296404dd1868ad
yotiosoft <ytani0323@gmail.com>
2026-02-11 10:13:30 +0900

    passed the heap test

--- src/allocator.rs
+++ src/allocator.rs
@@ -14,10 +14,12 @@ use bump::BumpAllocator;
 pub mod linked_list;
 use linked_list::LinkedListAllocator;
 
+use crate::allocator::fixed_size_block::FixedSizeBlockAllocator;
+
 pub mod fixed_size_block;
 
 #[global_allocator]
-static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;        // 100 KiB
